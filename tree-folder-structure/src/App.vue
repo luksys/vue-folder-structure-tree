861d68c92bfc6a3bb16a3b5512793a5eb5b6cd9c
EvaldasL <evaldas@pendulumcreative.uk>
2020-11-13 13:15:15 +0200

    - Updated the folders state to use nested folder structure instead of flat - Other updates and fixes

--- tree-folder-structure/src/App.vue
+++ tree-folder-structure/src/App.vue
@@ -3,7 +3,7 @@
     <v-main>
       <v-container>
         <h1 class="display-1 mb-4">Folders</h1>
-        <v-treeview :items="nestedFolders">
+        <v-treeview :items="folders" item-key="id">
           <template v-slot:prepend="{ item, open }">
             <v-icon>
               {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
@@ -12,7 +12,7 @@
           <template v-slot:label="{ item }">
             {{ item.name }}
             <v-btn
-              @click="createFolder(item.id)"
+              @click="createFolder(item.id, item.parentIds)"
               class="mx-2"
               tile
               x-small
@@ -30,53 +30,31 @@
 </template>
 
 <script>
-import { cloneDeep } from 'lodash'
-
 export default {
   name: 'App',
   computed: {
-    nestedFolders () {
-      const folders = cloneDeep(this.$store.state.folders)
-      return this.generateNestedFolders(folders)
+    folders () {
+      return this.$store.state.folders
     }
   },
   methods: {
-    createFolder (parentId) {
+    createFolder (id, parentIds) {
       const folderId = this.getNextFolderId()
+      const newParentIds = parentIds ? [...parentIds] : []
+      newParentIds.push(id)
       const folder = {
         id: folderId,
-        parentId: parentId,
+        parentIds: newParentIds,
         name: `Folder ${folderId}`
       }
       this.$store.dispatch('addFolder', folder)
     },
     getNextFolderId () {
-      if (this.folderIdTracker) return ++this.folderIdTracker
-
-      const folders = cloneDeep(this.$store.state.folders)
-      this.folderIdTracker = folders
-        .map(folder => folder.id)
-        .reduce((prev, current) => current > prev ? current : prev)
       return ++this.folderIdTracker
-    },
-    generateNestedFolders (folders, parentId) {
-      const parentFolders = folders.filter(folder => folder.parentId === parentId || folder.id === parentId)
-      parentFolders.map(parentFolder => {
-        const children = []
-        folders.forEach(folder => {
-          if (folder.parentId === parentFolder.id) {
-            children.push(this.generateNestedFolders(folders, folder.id)[0])
-          }
-        })
-        if (children.length) parentFolder.children = children
-        return parentFolder
-      })
-
-      return parentFolders
     }
   },
   data: () => ({
-    folderIdTracker: null
+    folderIdTracker: 7
   })
 }
 
--- tree-folder-structure/src/store/index.js
+++ tree-folder-structure/src/store/index.js
@@ -1,48 +1,84 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { cloneDeep } from 'lodash'
 
 Vue.use(Vuex)
-
-// const folders = [
-//   { id: 0, name: 'Folder 1', items: [
-//       { id: 1, parentId: 0, name: 'Folder 2' },
-//       { id: 2, parentId: 0, name: 'Folder 3', items: [
-//         { id: 3, parentId: 2, name: 'Folder 4' },
-//       ]}
-//   ]},
-//   { id: 0, name: 'Folder 1', items: [
-//     { id: 1, parentId: 0, name: 'Folder 2' },
-//     { id: 2, parentId: 0, name: 'Folder 3', items: [
-//         { id: 3, parentId: 2, name: 'Folder 4' },
-//       ]}
-//   ]}
-// ]
 
 export default new Vuex.Store({
   state: {
     folders: [
-      { id: 0, name: 'Folder 1' },
-      { id: 1, parentId: 0, name: 'Folder 2' },
-      { id: 2, parentId: 0, name: 'Folder 3' },
-      { id: 3, parentId: 2, name: 'Folder 4' },
-      { id: 4, parentId: 0, name: 'Folder 5' },
-      { id: 5, name: 'Folder 6' },
-      { id: 6, name: 'Folder 7' },
-      { id: 7, parentId: 6, name: 'Folder 8' }
+      {
+        id: 0,
+        name: 'Folder 0',
+        children: [
+          {
+            id: 1,
+            parentIds: [0],
+            name: 'Folder 1'
+          },
+          {
+            id: 2,
+            parentIds: [0],
+            name: 'Folder 2',
+            children: [
+              {
+                id: 3,
+                parentIds: [0, 2],
+                name: 'Folder 3'
+              }
+            ]
+          },
+          {
+            id: 4,
+            parentId: 0,
+            name: 'Folder 4'
+          }
+        ]
+      },
+      {
+        id: 5,
+        index: 1,
+        name: 'Folder 5'
+      },
+      {
+        id: 6,
+        index: 2,
+        name: 'Folder 6',
+        children: [
+          { id: 7, parentIds: [6], name: 'Folder 7' }
+        ]
+      }
     ]
   },
   mutations: {
     addFolder (state, folder) {
-      state.folders.push(folder)
+      const folders = cloneDeep(state.folders)
+      let currentFolder
+
+      if (folder.parentIds) {
+        folder.parentIds.forEach((parentId, index) => {
+          if (!currentFolder) {
+            currentFolder = folders.find(folderFromList => folderFromList.id === parentId)
+          } else {
+            currentFolder = currentFolder.children.find(folderFromList => folderFromList.id === parentId)
+          }
+
+          const lastItem = folder.parentIds.length - 1 === index
+
+          if (lastItem && !currentFolder.children) {
+            currentFolder.children = []
+          }
+        })
+
+        currentFolder.children.push(folder)
+      }
+
+      state.folders = folders
     }
   },
   actions: {
     addFolder (context, folder) {
       context.commit('addFolder', folder)
     }
-  },
-  modules: {
-  },
-  getters: {
   }
 })
